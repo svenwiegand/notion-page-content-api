e20d74da39f39b653d90bda2f0c7f447677041ac
Sven Wiegand <source@sven-wiegand.de>
2025-04-21 13:44:59 +0200

    Retrieving page markdown

--- src/functions/page.ts
+++ src/functions/page.ts
@@ -1,16 +1,22 @@
 import {app, HttpRequest, HttpResponseInit, InvocationContext} from "@azure/functions"
+import {Client} from "@notionhq/client"
+import {NotionToMarkdown} from "notion-to-md"
 
 async function html(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`)
-
-    const name = request.query.get('name') || await request.text() || 'world'
-    const ipAddress = request.headers.get('x-forwarded-for') || request.query.get("ip") || 'not available';
-    return {body: `Hello, ${name}!\n\nYour IP is ${ipAddress}`}
+    context.log(`Hola`)
+    const notion = new Client({
+        auth: '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',
+    })
+    const n2m = new NotionToMarkdown({notionClient: notion})
+    context.log(`Requesting notion page ${request.params.pageId}`)
+    const mdblocks = await n2m.pageToMarkdown(request.params.pageId)
+    const mdString = n2m.toMarkdownString(mdblocks).parent
+    return {body: mdString, headers: {'Content-Type': 'text/html'}}
 }
 
 app.http('html', {
     methods: ['GET'],
     authLevel: 'anonymous',
-    route: "page/{pageId:alpha}/html",
+    route: "page/{pageId}/html",
     handler: html,
 })
